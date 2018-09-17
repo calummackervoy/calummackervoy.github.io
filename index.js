515c3516b8a0addbb2faf195defdda819910a00b
Calum Mackervoy <c.mackervoy@gmail.com>
2018-09-17 14:42:49 +0100

    selecting a tag changes its styling to active

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
 
-function filter(tag) {
-    alert("Hello World! " + tag);
+var activeTag = "";
+
+//function toggles whether tag is active and applies filter
+function selectTag(tag) {
+    //update list of tags to reflect which is active
+    activateTag(tag);
+
+    activeTag = tag;
+
+    //display results
+    filter();
+}
+
+//function updates appearance of a tag in the list
+function activateTag(tag) {
+    //clear all tags in list of tag-active class
+    var items = document.getElementsByClassName('tag-item');
+    for(var i=0; i < items.length; i++) {
+        items[i].setAttribute('class', 'tag-item');
+    }
+
+    // set the selected tag's item to active
+    var item = document.getElementById(tag + '-item');
+    if(item) {
+        item.setAttribute('class', 'tag-item tag-active');
+    }
+}
+
+//function applies filters to projects, hiding any deselected
+function filter() {
+    //alert("active tag is " + activeTag);
 }
